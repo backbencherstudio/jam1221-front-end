722eb6f6d96ee896f7a7da1cdc87549f1bca2e37
saidul147 <sidul.islam.146@gmail.com>
2025-03-26 11:45:50 +0600

    for buil purpose some unused file have to comment out

--- app/quiz-platform/vehicle/page.tsx
+++ app/quiz-platform/vehicle/page.tsx
@@ -1,26 +1,26 @@
 "use client";
 
 
-import { useAuth } from '@/app/_components/AuthProviderContext';
-import { useRouter } from 'next/navigation';
+// import { useAuth } from '@/app/_components/AuthProviderContext';
+// import { useRouter } from 'next/navigation';
 import React, { useEffect } from 'react';
-import Loading from '../loading';
+// import Loading from '../loading';
 
 
 const VehicleQuestionPage = () => {
 
-    const route = useRouter()
-    const { isAuthenticated } = useAuth();
+    // const route = useRouter()
+    // const { isAuthenticated } = useAuth();
     
-    if(isAuthenticated === (false || null)){
-        return <Loading />
-    }
+    // if(isAuthenticated === (false || null)){
+    //     return <Loading />
+    // }
     
-    useEffect(() => {
-        if (isAuthenticated === false) {
-            route.replace("/login"); // or wherever you want to send authenticated users
-        }
-    }, [isAuthenticated]);
+    // useEffect(() => {
+    //     if (isAuthenticated === false) {
+    //         route.replace("/login"); // or wherever you want to send authenticated users
+    //     }
+    // }, [isAuthenticated]);
     
 
     return (
