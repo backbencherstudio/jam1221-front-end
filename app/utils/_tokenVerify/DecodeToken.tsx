076eea9621db8aedd699038d35c1729c96066ca9
tanvirAhmed73 <tanvir.073.ahmed@gmail.com>
2025-03-29 10:51:53 +0600

    error

--- app/utils/_tokenVerify/DecodeToken.tsx
+++ app/utils/_tokenVerify/DecodeToken.tsx
@@ -1,22 +1,22 @@
-import { jwtDecode } from "jwt-decode"; // ✅ CORRECT
+// // import { jwtDecode } from "jwt-decode"; // ✅ CORRECT
 
 
-interface DecodedToken {
-  exp: number;
-  name?: string;
-  sub?: string;
-  admin?: boolean;
-  // any other custom claims
-}
+// interface DecodedToken {
+//   exp: number;
+//   name?: string;
+//   sub?: string;
+//   admin?: boolean;
+//   // any other custom claims
+// }
 
-export const isTokenValid = (token: string): boolean => {
-  try {
-    const decoded = jwtDecode<DecodedToken>(token);
-    const currentTime = Date.now() / 1000;
+// export const isTokenValid = (token: string): boolean => {
+//   try {
+//     const decoded = jwtDecode<DecodedToken>(token);
+//     const currentTime = Date.now() / 1000;
 
-    return decoded.exp > currentTime;
-  } catch (err) {
-    console.error("Invalid token", err);
-    return false;
-  }
-};
+//     return decoded.exp > currentTime;
+//   } catch (err) {
+//     console.error("Invalid token", err);
+//     return false;
+//   }
+// };
